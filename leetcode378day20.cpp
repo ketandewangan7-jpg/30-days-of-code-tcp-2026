5b2ca6eed22f3f242269b22ff6e110c45b0bebeb
30-days-of-code-tcp-2026 <ketandewangan7@gmail.com>
2026-01-20 22:36:48 +0530

    leetcode 378 Odd Even Linked List day 20

---
+++ leetcode378day20.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+class Solution {
+public:
+    Node* oddEvenList(Node* head) {
+        if (head == NULL || head->next == NULL) return head;
+
+        Node* odd = head;
+        Node* even = head->next;
+        Node* evenHead = even;
+
+        while (even != NULL && even->next != NULL) {
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
