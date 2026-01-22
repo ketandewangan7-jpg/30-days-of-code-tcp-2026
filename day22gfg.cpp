604754f352e0c3cecac8304543090ece9374412f
30-days-of-code-tcp-2026 <ketandewangan7@gmail.com>
2026-01-22 22:54:40 +0530

    gfg question day22

---
+++ day22gfg.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <queue>
+using namespace std;
+
+/* inserts an element x at the back of the queue q */
+void push(queue<int> &q, int x) {
+    q.push(x);
+}
+
+/* pop out the front element from the queue q and returns it */
+int pop(queue<int> &q) {
+    if(q.empty()) return -1;   // safety check
+    int val = q.front();
+    q.pop();
+    return val;
+}
+
+/* returns the size of the queue q */
+int getSize(queue<int> &q) {
+    return q.size();
+}
+
+/* returns the last element of the queue */
+int getBack(queue<int> &q) {
+    if(q.empty()) return -1;
+    return q.back();
+}
+
+/* returns the first element of the queue */
+int getFront(queue<int> &q) {
+    if(q.empty()) return -1;
+    return q.front();
+}
